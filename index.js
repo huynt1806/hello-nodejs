10cbba816be878efef79b422ab2f064ff7abf88f
hamogoro <huynt1806@gmail.com>
2021-12-07 20:04:15 +0700

    add1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/about', function(req, res){
-  res.send('hello world');
+  res.send('hello world1');
 });
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!');
